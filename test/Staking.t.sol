1984f42bf859abae05562c3de875e0f99d8b1fda
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-08-11 22:36:31 +0400

    empty staking test

---
+++ test/Staking.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "../src/Staking.sol";
+import "../src/Token.sol";
+
+/**
+ * @title StakingTest Contract
+ * @author Nika Khachiashvili
+ * @dev Test cases for Staking Contract
+ * @dev IMPORTANT: These test cases aren't ready for production use, because not all the edge, tiny very specific cases are covered
+ */
+contract StakingTest is Test {
+
+}
